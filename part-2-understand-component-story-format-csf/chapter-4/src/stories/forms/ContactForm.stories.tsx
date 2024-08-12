51fec11435193d94bea2ebf3ba2fff6b5157463d
chakAs3 <javachakir@gmail.com>
2024-08-12 16:19:14 +0400

    add back title

--- part-2-understand-component-story-format-csf/chapter-4/src/stories/forms/ContactForm.stories.tsx
+++ part-2-understand-component-story-format-csf/chapter-4/src/stories/forms/ContactForm.stories.tsx
@@ -7,7 +7,7 @@ import { within, userEvent, expect } from '@storybook/test';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-
+  title: 'forms/ContactForm',
   component: ContactForm,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
