66f5c6ee01ce00113c6a8b90c25be19a30dcd132
chakAs3 <javachakir@gmail.com>
2024-08-12 15:52:39 +0400

    move Contact form to forms and remove title

--- part-2-understand-component-story-format-csf/chapter-4/src/stories/ContactForm.stories.tsx
+++ part-2-understand-component-story-format-csf/chapter-4/src/stories/ContactForm.stories.tsx
@@ -1,13 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { ContactForm } from './ContactForm';
+import { ContactForm } from './forms/ContactForm';
 import { SocialLinks , Copyright } from './components/Footer';
 import { within, userEvent, expect } from '@storybook/test';
 
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'forms/ContactForm',
   component: ContactForm,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
