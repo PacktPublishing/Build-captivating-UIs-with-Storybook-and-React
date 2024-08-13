c30079d60d07712b45b64b84103925bc1078110c
chakAs3 <javachakir@gmail.com>
2024-08-13 18:23:16 +0400

    chapter 5-  theme switcher implementation

---
+++ part-2-understand-component-story-format-csf/chapter-5-theme-switcher/.storybook/preview.ts
@@ -0,0 +1,25 @@
+import type { Preview } from "@storybook/react";
+import { withThemeByClassName } from "@storybook/addon-themes"
+import '../src/index.css';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: 'light-theme',
+        dark: 'dark-theme',
+      },
+      defaultTheme: 'light',
+    }),
+  ],
+};
+
+export default preview;
