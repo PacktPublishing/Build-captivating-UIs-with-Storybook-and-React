ce322d44d715b73354ed1de1a6732aff5386267e
chakAs3 <javachakir@gmail.com>
2024-08-17 19:44:23 +0400

    chapter 5 add extra custimzations to the theme

--- part-2-understand-component-story-format-csf/chapter-5-custom-sb/.storybook/PacktTheme.ts
+++ part-2-understand-component-story-format-csf/chapter-5-custom-sb/.storybook/PacktTheme.ts
@@ -6,4 +6,32 @@ export default create({
   brandUrl: 'https://www.packtpub.com/', 
   brandImage: 'https://www.packtpub.com/rebuild/build/assets/logo-new-Dz-8EKdV.svg',
   brandTarget: '_self',
+
+  // extra customizations
+  colorPrimary: '#f97141',
+  colorSecondary: '#585C6D',
+
+  // UI
+  appBg: '#f2f2f2',
+  appContentBg: '#f2f2f2',
+  appPreviewBg: '#f2f2f2',
+  appBorderColor: '#585C6D',
+  appBorderRadius: 4,
+
+  // Text colors
+  textColor: '#10162F',
+  textInverseColor: '#ffffff',
+
+  // Toolbar default and active colors
+  barTextColor: '#9E9E9E',
+  barSelectedColor: '#585C6D',
+  barHoverColor: '#585C6D',
+  barBg: '#ffffff',
+
+  // Form colors
+  inputBg: '#ffffff',
+  inputBorder: '#10162F',
+  inputTextColor: '#10162F',
+  inputBorderRadius: 2,
+
 });
